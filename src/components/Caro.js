583abf4a59e44c625a69b94e61de0c18aaa0b508
Colin Edwards <ColinJhEdwards@gmail.com>
2021-09-06 20:46:26 -0700

    added images to carousel and removed text, also added icons for van features

--- src/components/Caro.js
+++ src/components/Caro.js
@@ -1,46 +1,20 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import van from "../images/5.jpg";
+import van1 from "../images/5.jpg";
+import van2 from "../images/6.jpg";
+import van3 from "../images/1.jpg";
 
 const Caro = () => {
   return (
-    <Carousel>
+    <Carousel fade nextLabel={null} prevLabel={null}>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <img className="d-block w-100" src={van1} alt="First slide" />
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <img className="d-block w-100" src={van2} alt="Second slide" />
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+        <img className="d-block w-100" src={van3} alt="Third slide" />
       </Carousel.Item>
     </Carousel>
   );
--- src/components/TheVan.js
+++ src/components/TheVan.js
@@ -2,13 +2,38 @@ import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import Caro from "./Caro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faUserFriends,
+  faGasPump,
+  faTachometerAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const TheVan = () => {
   return (
     <StyledVan>
       <h1>Designed with sustainabilty in mind.</h1>
       <hr />
-      <Caro />
+      <div className="carousel">
+        <Caro />
+      </div>
+      <div className="icons">
+        <FontAwesomeIcon icon={faBed} />
+        <FontAwesomeIcon icon={faUserFriends} />
+        <FontAwesomeIcon icon={faGasPump} />
+        <FontAwesomeIcon icon={faTachometerAlt} />
+      </div>
+      <p>
+        A 2007 Ford Econoline 150 equipped with solar panels, remodeled with
+        sustainable material, and added storage made from cedar wood which
+        creates a lovely smell that deters bugs. Lorem ipsum, dolor sit amet
+        consectetur adipisicing elit. Maiores fuga eos at nihil. Illo quos, et
+        quasi ducimus maxime commodi perferendis laborum, veritatis quia fugiat
+        aperiam voluptate odio molestias. Incidunt repellat facilis voluptas sed
+        placeat ratione, eligendi obcaecati quam reprehenderit aliquid
+        dignissimos iure animi porro odit quia provident earum rerum!
+      </p>
     </StyledVan>
   );
 };
@@ -20,11 +45,20 @@ const StyledVan = styled(motion.div)`
   align-items: center;
   justify-content: space-around;
   hr {
-    width: 60%;
-    height: 2px;
+    width: 10%;
+    height: 4px;
+    background: #630a10;
+    opacity: 1;
   }
-  img {
-    width: 50%;
+  .carousel {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 70%;
+  }
+  p {
+    text-align: center;
+    width: 60%;
   }
 `;
 
