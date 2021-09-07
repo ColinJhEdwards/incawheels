85798233db8770912b5da4b81ed4c6b188b44b30
Colin Edwards <ColinJhEdwards@gmail.com>
2021-09-06 20:26:24 -0700

    added bootstrap carousel to display images of van

--- src/components/TheVan.js
+++ src/components/TheVan.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import Caro from "./Caro";
 
 const TheVan = () => {
   return (
     <StyledVan>
       <h1>Designed with sustainabilty in mind.</h1>
+      <hr />
+      <Caro />
     </StyledVan>
   );
 };
@@ -15,6 +18,14 @@ const StyledVan = styled(motion.div)`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-around;
+  hr {
+    width: 60%;
+    height: 2px;
+  }
+  img {
+    width: 50%;
+  }
 `;
 
 export default TheVan;
